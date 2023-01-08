5e2dbef156e59a10f4a021f2c961bbe694350c16
NataliaMartynuik <98057592+NataliaMartynuik@users.noreply.github.com>
2023-01-09 00:39:59 +0200

    зроблено деструктуризацію

--- controllers/contacts.controller.js
+++ controllers/contacts.controller.js
@@ -1,14 +1,14 @@
-const contactsDb = require('../models/contacts');
-const { HttpError } = require('../helpers/index');
+const { listContacts, getContactById, removeContact, addContact, updateContact } = require('../models/contacts.js');
+const { HttpError } = require('../helpers/index.js');
 
 async function getContacts (req, res) {
-  const contacts = await contactsDb.listContacts();
+  const contacts = await listContacts();
   res.json(contacts);
 }
 
 async function getContact (req, res, next) {
   const { contactId } = req.params;
-  const contact = await contactsDb.getContactById(contactId)
+  const contact = await getContactById(contactId)
   if (!contact) {
     return next(HttpError(404, `Contact with ID ${contactId} not found!`))
   }
@@ -17,24 +17,24 @@ async function getContact (req, res, next) {
 
 async function createContact (req, res) {
   const { name, email, phone } = req.body;
-  const newContact = await contactsDb.addContact(name, email, phone);
+  const newContact = await addContact(name, email, phone);
   res.status(201).json(newContact);
 }
 
 async function deleteContact (req, res, next) {
   const { contactId } = req.params;
-  const contact = await contactsDb.getContactById(contactId);
+  const contact = await getContactById(contactId);
   if (!contact) {
     return next(HttpError(404, `Contact with ID ${contactId} not found!`))
   }
-  await contactsDb.removeContact(contactId);
+  await removeContact(contactId);
   res.status(200).json(contact);
 }
 
 async function putContact (req, res, next) {
   const { contactId } = req.params;
   const { name, email, phone } = req.body;
-  const contact = await contactsDb.updateContact(contactId, name, email, phone);
+  const contact = await updateContact(contactId, name, email, phone);
   if (!contact) {
     return next(HttpError(404, `Contact with ID ${contactId} not found!`))
   }
